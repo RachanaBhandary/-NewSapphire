db75e45cbfbb70807989bcb96f1bfb17d994dc68
slk <kinishravan98@gmail.com>
2019-12-05 18:07:51 +0530

    aa

--- src/main/java/com/example/demo/BankSapphireApplication.java
+++ src/main/java/com/example/demo/BankSapphireApplication.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -10,10 +12,15 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @ComponentScan({"com.slk.*"})
 @SpringBootApplication
-public class BankSapphireApplication {
+public class BankSapphireApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankSapphireApplication.class, args);
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder)
+	{
+		return builder.sources(BankSapphireApplication.class);
 	}
 
 }
